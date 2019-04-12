c09ba84639f3302326213159cb425027f41a3ce2
Anish <anish8129@gmail.com>
2019-04-13 02:19:59 +0530

    initial commit

---
+++ src/main/java/com/Heroku/MyController.java
@@ -0,0 +1,14 @@
+package com.Heroku;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	
+	@RequestMapping("/test")
+	public String hello() {
+		return "Hello World";
+	}
+
+}
